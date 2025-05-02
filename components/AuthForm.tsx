0a497e6680d9b786e65924e259ece1270c4cff91
Hellnawwh <mehulsamal123@gmail.com>
2025-05-02 20:36:57 +0530

    Copied code from the source(in root layout and page)

--- components/AuthForm.tsx
+++ components/AuthForm.tsx
@@ -6,7 +6,7 @@
  
  import { Button } from "@/components/ui/button"
  import { Form} from "@/components/ui/form"
- import Image from "next/image";
+//  import Image from "next/image";
  import Link from "next/link";
  import {toast} from "sonner";
  import FormField from "@/components/FormField";
